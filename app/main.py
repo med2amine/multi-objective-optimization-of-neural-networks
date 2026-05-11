efeb0e63aeeaa7c5441919d470690a5039575284
LAARAJ Mohamed Amine <mohamedamine.laaraj@cgi.com>
2026-05-11 15:29:43 +0100

    added the path to the style file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 import sys 
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
+from pathlib import Path
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+
+    qss_path = Path(__file__).parent /"style"/ "style.qss"
+    app.setStyleSheet(qss_path.read_text())
+
     window.show()
     sys.exit(app.exec())
